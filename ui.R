9c640591ac9493d0b495518c126c9e2ae816604a
Yuki Yanai <yanai.yuki@kochi-tech.ac.jp>
2021-11-15 07:32:25 +0900

    adds Bayes rule

--- ui.R
+++ ui.R
@@ -17,11 +17,16 @@ shinyUI(fluidPage(
            img(src = "yanai_lab_logo.png", height = 120),
            br(),
            br()),
+    column(1),
+    column(4,
+           h4("ベイズルール"),
+           helpText(withMathJax("$$p(\\theta \\mid y) \\propto p(y \\mid \\theta) p(\\theta)$$"))
+           ),
     column(4,
            h4("ベータ-二項分布モデル"),
            helpText(withMathJax("$$Y \\sim \\mbox{Binomial}(n, \\theta)$$")),
-           helpText(withMathJax("$$\\theta \\sim \\mbox{Beta}(\\alpha, \\beta)$$"))),
-    column(5)
+           helpText(withMathJax("$$\\theta \\sim \\mbox{Beta}(\\alpha, \\beta)$$")),
+    ),
   ),
 
   fluidRow(
@@ -37,6 +42,16 @@ shinyUI(fluidPage(
            br(),
            actionButton("reset", "リセット")),
     column(4,
+           br(),
+           actionButton("update", "データを抽出してベイズ更新"),
+           br(),
+           br(),
+           textOutput("trials"),
+           textOutput("y"),
+           br(),
+           h4("データ"),
+           textOutput("data")),
+    column(4,
            h4("事前分布の設定"),
            numericInput("alpha",
                         withMathJax("$$\\alpha \\in [0.01, 100]$$"),
@@ -47,30 +62,30 @@ shinyUI(fluidPage(
                         withMathJax("$$\\beta \\in [0.01, 100]$$"),
                         min = 0.01,
                         max = 100,
-                        value = 1)),
-    column(4,
-           br(),
-           actionButton("update", "データを抽出してベイズ更新"),
-           br(),
-           br(),
-           textOutput("trials"),
-           textOutput("y"),
-           br(),
-           h4("データ"),
-           textOutput("data"))
+                        value = 1))
   ),
-  
-  
+
   fluidRow(
       column(4,
              h3("事後分布"),
-             plotOutput("posterior")),
-    column(4,
-           h3("事前分布"),
-           plotOutput("prior")),
-    column(4,
-           h3("尤度"),
-           plotOutput("likelihood"))
+             p(withMathJax("$$p(\\theta \\mid y)$$")),
+             plotOutput("posterior"),
+             p(withMathJax("$$\\theta \\mid y \\sim \\mbox{Beta}(y + \\alpha, n - y + \\beta)$$")),
+             br()
+             ),
+      column(4,
+             h3("尤度"),
+             p(withMathJax("$$p(y \\mid \\theta) = L(\\theta \\mid y)$$")),
+             plotOutput("likelihood"),
+             p(withMathJax("$$y \\mid \\theta \\sim \\mbox{Binomial}(n, \\theta)$$")),
+             br()
+             ),
+      column(4,
+             h3("事前分布"),
+             p(withMathJax("$$p(\\theta)$$")),
+             plotOutput("prior"),
+             p(withMathJax("$$\\theta \\sim \\mbox{Beta}(\\alpha, \\beta)$$")),
+             br())
   )
     
 ))
